6686a703121590fec8afebef50fc74562c74fea0
arche578 <styteller178@gmail.com>
2025-11-07 09:24:41 +0900

    font change test

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "Sans serif",
         code: "IBM Plex Mono",
       },
       colors: {
